22defa0666668fee894ed580a4f9abf286028f98
camih1 <cami.h1@hotmail.com>
2018-09-21 21:03:55 -0300

    Update testes_inic.py

--- src/testes_inic.py
+++ src/testes_inic.py
@@ -10,14 +10,14 @@ if len(jogo) != 3:
   erroInicializar = True
 else:
   for linha in jogo:
-    if len(linha) != 3:
-      erroInicializar = True
-    else:
-      for elemento in linha:
-        if elemento != '.':
-          erroInicializar = True
+         if len(linha) != 3:
+                   erroInicializar = True
+         else:
+            for elemento in linha:
+                   if elemento != '.':
+                            erroInicializar = True
 if erroInicializar:
-    print('Erro!')
-    sys.exit(1)
+          print('Erro!')
+          sys.exit(1)
 else:
-    sys.exit(0)
+          sys.exit(0)
